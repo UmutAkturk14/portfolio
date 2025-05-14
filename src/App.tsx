0cef0c47fac57f75871fa1a2adec3672128bb0b5
Umut Akturk <umut.akturk14@gmail.com>
2025-05-14 03:51:12 +0300

    Fixing problems

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,8 @@
-import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import { fetchTranslations } from "./utils/translationFetcher";
 import Hero from "./components/sections/Hero";
 
 const App = () => {
-  const [count, setCount] = useState(0);
-  fetchTranslations();
-
-  const handleClick = () => {
-    setCount((count) => {
-      return count + 1;
-    });
-  };
-
   return (
     <>
       <Navbar />
