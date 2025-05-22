c85839a140f46a484826657d61e84b305c75dc53
Umut Akturk <umut.akturk14@gmail.com>
2025-05-22 03:47:46 +0300

    Fixed an element

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,8 @@
 const Footer = () => {
   return (
-    <footer className="py-8 font-heading text-xl flex justify-center items-center text-primary underline underline-offset-8 select-none"></footer>
+    <footer className="py-8 font-heading text-xl flex justify-center items-center text-primary underline underline-offset-8 select-none">
+      <p>&copy; Umut Akturk 2025</p>
+    </footer>
   );
 };
 
